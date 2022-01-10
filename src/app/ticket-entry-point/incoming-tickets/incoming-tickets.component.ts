3b045fcd7cdb6455e48a2cd8ee4605f873ff6afb
Erwin-Feld <erwinfeld.dev@gmail.com>
2022-01-10 15:30:23 +0100

    subscribed to Observable provided by service changed name of property incomingTickets to receaveTickets

--- src/app/ticket-entry-point/incoming-tickets/incoming-tickets.component.ts
+++ src/app/ticket-entry-point/incoming-tickets/incoming-tickets.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TicketDistributorService } from 'src/app/ticket-distributor.service';
 import { TicketData } from '../interfaces/ticket-data.interface';
-import mockTicketData from '../mock-data/mock-tickets.json'
+// import mockTicketData from '../mock-data/mock-tickets.json'
 
 
 
@@ -11,10 +12,19 @@ import mockTicketData from '../mock-data/mock-tickets.json'
 })
 export class IncomingTicketsComponent implements OnInit {
 
-  incomingTickets: TicketData[] = mockTicketData 
-  constructor() { }
+  // incomingTickets: TicketData[] = mockTicketData; 
+  
+  receaveTickets: TicketData[] = [];
+  
+  constructor(private ticketDistributorService: TicketDistributorService) { }
 
   ngOnInit(): void {
+    this.getTicketData();
+  }
+
+  getTicketData():void {
+    this.ticketDistributorService.getTicketData().
+                          subscribe(receaveTickets => this.receaveTickets = receaveTickets)
   }
 
 }
